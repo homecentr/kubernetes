86a05a7972bd49bf3d9663d38e793768652e0d65
Lukas Holota <lholota@vistaprint.com>
2023-01-12 11:09:35 +0100

    Scripts and SOPS/helm_secrets sample

---
+++ scripts/key-show.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+. ./scripts/key-parse.sh $1
+
+echo "Public key"
+echo "--------------------------------------------------"
+echo "Raw:    $SOPS_AGE_RECIPIENTS"
+echo "Base64: $(echo $SOPS_AGE_RECIPIENTS | base64)"
+echo ""
+echo "Private key"
+echo "--------------------------------------------------"
+echo "Raw:    $SOPS_AGE_KEY"
+echo "Base64: $(echo $SOPS_AGE_KEY | base64)"
--- scripts/secrets-edit.sh
+++ scripts/secrets-edit.sh
@@ -2,19 +2,10 @@
 
 set -e
 
-printHelp() {
-  echo "Usage: secrets-edit.sh (lab|prod)"
-}
+# Load the age key
+. ./scripts/key-parse.sh $1
 
-ENVIRONMENT=$1
-EDITOR=nano
-
-# Decrypt keys with GPG (yubikey)
-AGE_KEYS=$(gpg --batch --use-agent --decrypt ./.keys/$ENVIRONMENT.gpg)
-
-# Parse the age keys
-SOPS_AGE_RECIPIENTS=$(echo "$AGE_KEYS" | grep public | awk -F': ' '{print $2}')
-SOPS_AGE_KEY=$(echo "$AGE_KEYS" | grep -v "#")
+export EDITOR=nano
 
 # Edit the secrets file
 sops -i "$INIT_CWD/secrets-$1.yml"
